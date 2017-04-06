011c58f68f6f0c678856b6b27010f28e620c13ef
studente <studente@Mat-26>
2017-04-06 11:20:47 +0200

    Finche la barca va

---
+++ Finchelabarcava/src/finchelabarcava/Finchelabarcava.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finchelabarcava;
+
+/**
+ *
+ * @author studente
+ */
+public class Finchelabarcava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Finestra finestra;
+        finestra = new Finestra();
+        finestra.setVisible(true);
+        finestra.setTitle("Fince la barca va");
+        
+    }
+
+}
